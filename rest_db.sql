469a038c49fd289715618481ffd03f68f9018b7e
pavan kumar <pavan.bizzmanweb@gmail.com>
2024-02-07 14:19:41 +0530

    added database file

---
+++ rest_db.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 07, 2024 at 08:08 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rest_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+-- Error reading structure for table rest_db.admins: #1932 - Table &#039;rest_db.admins&#039; doesn&#039;t exist in engine
+-- Error reading data for table rest_db.admins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `rest_db`.`admins`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+-- Error reading structure for table rest_db.booking: #1932 - Table &#039;rest_db.booking&#039; doesn&#039;t exist in engine
+-- Error reading data for table rest_db.booking: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `rest_db`.`booking`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+-- Error reading structure for table rest_db.cart: #1932 - Table &#039;rest_db.cart&#039; doesn&#039;t exist in engine
+-- Error reading data for table rest_db.cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `rest_db`.`cart`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foods`
+--
+-- Error reading structure for table rest_db.foods: #1932 - Table &#039;rest_db.foods&#039; doesn&#039;t exist in engine
+-- Error reading data for table rest_db.foods: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `rest_db`.`foods`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table rest_db.orders: #1932 - Table &#039;rest_db.orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table rest_db.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `rest_db`.`orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+-- Error reading structure for table rest_db.reviews: #1932 - Table &#039;rest_db.reviews&#039; doesn&#039;t exist in engine
+-- Error reading data for table rest_db.reviews: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `rest_db`.`reviews`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table rest_db.users: #1932 - Table &#039;rest_db.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table rest_db.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `rest_db`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
